26ae250da9132c1b5d69dadfe323149073f8ffb2
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-05-12 19:20:41 +0200

    karta szczegółowa

---
+++ src/app/api/books/[id]/route.ts
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+import { Book } from '@/types/book';
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const bookId = params.id;
+
+    const response = await fetch(
+      `https://www.googleapis.com/books/v1/volumes/${bookId}`,
+    );
+    if (!response.ok) {
+      throw new Error('Failed to fetch data from Google Books API');
+    }
+
+    const data = await response.json();
+
+    const volumeInfo = data.volumeInfo;
+
+    const book: Book = {
+      id: data.id,
+      title: volumeInfo.title || 'No title available',
+      authors: volumeInfo.authors || ['Unknown author'],
+      publishedDate: volumeInfo.publishedDate || 'Unknown date',
+      averageRating: volumeInfo.averageRating,
+      categories: volumeInfo.categories || ['Uncategorized'],
+      description: volumeInfo.description
+        ? `<p>${volumeInfo.description}</p>`
+        : '<p>No description available.</p>',
+      thumbnail: volumeInfo.imageLinks?.thumbnail || '/book-covers/default-cover.svg',
+    };
+
+    return NextResponse.json({ book }, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching book details:', error);
+    return NextResponse.json(
+      { error: 'Failed to fetch book details' },
+      { status: 500 },
+    );
+  }
+}
