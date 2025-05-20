9b27f072bb424d04dad8a43d6fc78d922dd70a91
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-05-20 17:07:55 +0200

    endpoint z listą ksiązek z ich recenzjami

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,33 @@
 import { NextResponse } from 'next/server';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '@/lib/firebase/firebase';
 
 export async function GET() {
-  return NextResponse.json({
-    hello: 'world',
-  });
+  try {
+    const booksSnapshot = await getDocs(collection(db, 'books'));
+
+    const booksWithRatings = await Promise.all(
+      booksSnapshot.docs.map(async (bookDoc) => {
+        const bookId = bookDoc.id;
+        const bookData = bookDoc.data();
+
+        const ratingsSnapshot = await getDocs(collection(db, 'books', bookId, 'ratings'));
+        const ratings = ratingsSnapshot.docs.map(ratingDoc => ({
+          id: ratingDoc.id,
+          ...ratingDoc.data()
+        }));
+
+        return {
+          id: bookId,
+          ...bookData,
+          ratings
+        };
+      })
+    );
+
+    return NextResponse.json(booksWithRatings);
+  } catch (error) {
+    console.error('Błąd API:', error);
+    return NextResponse.json({ error: 'Wystąpił błąd serwera' }, { status: 500 });
+  }
 }
