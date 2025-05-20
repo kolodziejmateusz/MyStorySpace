4bcb38b3935c2e24683d0e6649e5b8099c6518d2
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2025-05-20 12:49:59 +0200

    testowy dialog

---
+++ src/app/(public)/test/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CurrentBookPageDialog from '@/components/ui/CurrentBookPageDialog'
+
+export default function test() {
+  return (
+    <div className='p-16'><CurrentBookPageDialog/></div>
+  )
+}
